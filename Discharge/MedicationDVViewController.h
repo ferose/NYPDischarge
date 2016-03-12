4b79496422053835749c09cd1744c197ddd8f554
Henna <henna.ahmed92@gmail.com>
2016-03-12 17:14:53 -0500

    fixing drug detail view

---
+++ Discharge/MedicationDVViewController.h
@@ -0,0 +1,13 @@
+//
+//  MedicationDVViewController.h
+//  Discharge
+//
+//  Created by Henna Ahmed on 3/12/16.
+//  Copyright © 2016 NYP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MedicationDVViewController : UIViewController
+
+@end
