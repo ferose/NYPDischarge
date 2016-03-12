8e7071985c8ebe2f00f7b536863c057b42ab48e5
Ferose Babu <ferose2@gmail.com>
2016-03-12 11:09:37 -0500

    user stub

---
+++ Discharge/User.h
@@ -0,0 +1,15 @@
+//
+//  User.h
+//  Discharge
+//
+//  Created by Ferose Babu on 3/12/16.
+//  Copyright © 2016 NYP. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+
++(instancetype)currentUser;
+
+@end
---
+++ Discharge/User.m
@@ -0,0 +1,24 @@
+//
+//  User.m
+//  Discharge
+//
+//  Created by Ferose Babu on 3/12/16.
+//  Copyright © 2016 NYP. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
++(instancetype)currentUser
+{
+    static User *shared = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        shared = [[self alloc] init];
+    });
+    
+    return shared;
+}
+
+@end
