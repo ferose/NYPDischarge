82beb95d823ac76abedcf6b22d6d32ca2a5e7ef3
Ferose Babu <ferose2@gmail.com>
2016-03-12 12:20:34 -0500

    meds controller stub

---
+++ Discharge/MedsMasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MedsMasterViewController.h
+//  Discharge
+//
+//  Created by Ferose Babu on 3/12/16.
+//  Copyright © 2016 NYP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MedsMasterViewController : UIViewController
+
+@end
---
+++ Discharge/MedsMasterViewController.m
@@ -0,0 +1,37 @@
+//
+//  MedsMasterViewController.m
+//  Discharge
+//
+//  Created by Ferose Babu on 3/12/16.
+//  Copyright © 2016 NYP. All rights reserved.
+//
+
+#import "MedsMasterViewController.h"
+
+@interface MedsMasterViewController ()
+
+@end
+
+@implementation MedsMasterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
