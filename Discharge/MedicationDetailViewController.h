9cae6de89972fcb36f5c61f2a210baf95c589a32
jamaal51 <jsedayao@gmail.com>
2016-03-12 16:27:08 -0500

    Added Medication Detail View Controller file

---
+++ Discharge/MedicationDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  MedicationDetailViewController.h
+//  Discharge
+//
+//  Created by Jamaal Sedayao on 3/12/16.
+//  Copyright © 2016 NYP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MedicationDetailViewController : UIViewController
+
+@end
