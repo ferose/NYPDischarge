1b76471bbb21a1cb24950acc0ca06e5597f7b3e8
Henna <henna.ahmed92@gmail.com>
2016-03-13 13:17:23 -0400

    my changes

---
+++ Discharge/Encounter.h
@@ -0,0 +1,14 @@
+//
+//  Encounter.h
+//  Discharge
+//
+//  Created by Henna Ahmed on 3/13/16.
+//  Copyright © 2016 NYP. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Encounter : NSObject
+@property (nonatomic) NSString * date;
+@property (nonatomic) NSString * cause;
+@end
