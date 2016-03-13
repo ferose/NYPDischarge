9310dfe1ecb865a286f81072ac13d44059214f8b
Ferose Babu <ferose2@gmail.com>
2016-03-13 13:45:21 -0400

    contacts refactor

---
+++ Discharge/ContactsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContactsTableViewController.h
+//  Discharge
+//
+//  Created by Ferose Babu on 3/13/16.
+//  Copyright © 2016 NYP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContactsTableViewController : UITableViewController
+
+@end
